f91f7cccc0e818bb51ee5ef615bebe3ef880bffc
U-ROMULUS\Michael <michael.wolfendale@gmail.com>
2015-10-11 19:05:14 +0100

    initial edit iteration code

--- global/admin/app.js
+++ global/admin/app.js
@@ -9,15 +9,6 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-// This would reset the app in a really
-// really dirty way. Not sure if we need this
-// app.get('/touch', function (req, res) {
-//  setTimeout(function () {
-//    touch(path.join(cwd, 'start.js'));
-//  }, 1000);
-//  res.redirect('/');
-// });
-
 app.get('/new-iteration', function (req, res) {
   res.render('new-iteration', {
     apps : require('../../lib/models/apps-model.js')
@@ -26,12 +17,20 @@ app.get('/new-iteration', function (req, res) {
 
 app.post('/new-iteration', function (req, res) {
   var body = req.body;
-  console.log(path.join(cwd, 'app', body.app));
-  // fs.copySync(
-  //   path.join(cwd, 'app', body.app),
-  //   path.join(cwd, 'app', body.path)
-  // );
+  var meta = {
+    name : body.name,
+    description : body.description
+  };
+  fs.copySync(
+    path.join(cwd, 'app', body.app),
+    path.join(cwd, 'app', body.path)
+  );
+  fs.writeJsonSync(
+    path.join(cwd, 'app', body.path, 'meta.json'), meta);
   res.redirect('new-iteration');
+  setTimeout(function () {
+    touch(path.join(cwd, 'start.js'));
+  }, 1000);
 });
 
 module.exports = app;
